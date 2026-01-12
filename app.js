be0ca43ddc9c52338c15eb1660deb205096ddad5
Amanda Steeples <amandasteeples304@gmail.com>
2026-01-12 11:51:02 +0000

    withcalculate

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ function subtract(a, b) {
 }
 
 const solution = subtract(1, 5);
-console.log("The result is", answer);
+console.log("The result is", solution);
 const anotherSolution = subtract(2, 10);
 console.log("Another result is", anotherSolution);
 
@@ -55,6 +55,28 @@ function divide(a, b) {
 }
 
 const retort = divide(1, 5);
-console.log("The result is", answer);
+console.log("The result is", retort);
 const anotherRetort = divide(1, 5);
 console.log("Another result is", anotherRetort);
+
+function calculate(a, b, operator) {
+  if (operator === "+") {
+    return add(a, b);
+  } else if (operator === "-") {
+    return subtract(a, b);
+  } else if (operator === "*") {
+    return multiply(a, b);
+  } else if (operator === "/") {
+    return divide(a, b);
+  }
+}
+
+const product = calculate(1, 5, "+");
+console.log("The result is", product);
+const anotherProduct = calculate(2, 10, "*");
+console.log("Another result is", anotherProduct);
+
+const minusproduct = calculate(10, 2, "-");
+console.log("The result is", minusproduct);
+const divideproduct = calculate(20, 10, "/");
+console.log("Another result is", divideproduct);
