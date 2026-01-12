88b2d4420554922857e28d2f2270e1ae423476e6
Amanda Steeples <amandasteeples304@gmail.com>
2026-01-12 11:35:28 +0000

    withreturnwithoutcalculate

--- app.js
+++ app.js
@@ -22,3 +22,39 @@ function greet(greeting, userName) {
 greet("Good morning", "Bob");
 greet("Buenos dias", "Alice");
 greet("Bonjour", "Charlie");
+
+function add(a, b) {
+  return a + b;
+}
+
+const result = add(1, 5);
+console.log("The result is", result);
+const anotherResult = add(2, 10);
+console.log("Another result is", anotherResult);
+
+function multiply(a, b) {
+  return a * b;
+}
+
+const answer = multiply(1, 5);
+console.log("The result is", answer);
+const anotherAnswer = multiply(2, 10);
+console.log("Another result is", anotherAnswer);
+
+function subtract(a, b) {
+  return a - b;
+}
+
+const solution = subtract(1, 5);
+console.log("The result is", answer);
+const anotherSolution = subtract(2, 10);
+console.log("Another result is", anotherSolution);
+
+function divide(a, b) {
+  return a / b;
+}
+
+const retort = divide(1, 5);
+console.log("The result is", answer);
+const anotherRetort = divide(1, 5);
+console.log("Another result is", anotherRetort);
